dd5aeeb18042723b00e8c7da6ff527c4e2a3a74a
郭寒冰 <3358644761@qq.com>
2022-11-16 19:49:38 +0800

    封装单独的登录接口

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,11 @@
-// import request from '@/utils/request'
+import request from '@/utils/request'
 
+/* *
+* 登录接口
+ */// 因为所有的接口都要跨域 表示所有的接口要带 /api
 export function login(data) {
-
+  // 返回一个axios对象 => promise  // 返回了一个promise对象
+  return request.post('/sys/login', data)
 }
 
 export function getInfo(token) {
