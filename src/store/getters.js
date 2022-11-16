c1d526af5655fbf4e332df0eca83dbefddde064d
郭寒冰 <3358644761@qq.com>
2022-11-16 20:26:23 +0800

    登录页面调用登录action，处理异常

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,8 +2,8 @@ const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
   // 在根级的getters上 开发子模块的属性给别人看 给别人用
-  token: state => state.user.token
-  // avatar: state => state.user.avatar,
-  // name: state => state.user.name
+  token: state => state.user.token,
+  avatar: state => state.user.avatar,
+  name: state => state.user.name
 }
 export default getters
